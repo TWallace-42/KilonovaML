a4897e23f8d08164a71f08fef9409caf666a3a6f
Thomas Wallace <91464484+2300431w@users.noreply.github.com>
2022-04-05 17:03:36 +0100

    comments added to FlowModelResultsHist.py

--- FlowModelResultsHist.py
+++ FlowModelResultsHist.py
@@ -14,43 +14,20 @@ from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-#settings
+#--------------------------#
+#uses a machine learning model to create a histogram of the predictions made for a variety of inputs
+
 plt.style.use('seaborn-colorblind')
-cmap = cm.get_cmap('viridis')
+cmap = cm.get_cmap('viridis') #define the colourmap
 N_samples = 100 #number of lines to generate
+fname = 'NS-priors.txt'#the txt file holding the NS-NS 170817 priors for our tests
 
-#parameters
-
-
-"""Total_M = 2.83 #+0.04,-0.01 uncertainty
-
-f_min = 0.40989399293
-f_max = 0.56537102473 #mass is constrained between 1.16 and 1.6 => these are the constraints on f
-
-f = random.uniform(f_min,f_max)#0.5 
-m1 = f*Total_M
-m2 = (1-f)*Total_M
-#lambda (tidal deformabilities) is between 0 and 630
-l1 = 300 #+420,-230 uncertainty
-l2 = 300
-"""
-
-"""f = h5py.File("GW170817_GWTC-1.hdf5",'r')
-#print(list(f.keys()))
-data = f['IMRPhenomPv2NRT_highSpin_posterior']
-m1_array = data['m1_detector_frame_Msun']
-m2_array = data['m2_detector_frame_Msun']
-l1_array = data['lambda1']
-l2_array = data['lambda2']
-
-m1 = np.mean(m1_array)
-m2 = np.mean(m2_array)
-l1 = np.mean(l1_array)
-l2 = np.mean(l2_array)"""
-
-with open('NS-priors.txt','r') as file:
+with open(fname,'r') as file:
+    #open the file and read the lines
     data2 = file.readlines()
-
+    
+#format the data in a way more useful for our analysis.
+#Could probably be optimised but doesn't seem necessary as this is just a diagnostic tool
 final_dat2 = []
 for d in data2:
     m1,m2,l1,l2 = d.split()[0:4]
@@ -73,19 +50,16 @@ l2 = np.mean(l2_array)
 print(f'm1: {m1}\tm2: {m2}\tl1: {l1}\tl2: {l2}')
 
 
-
+#create an array of conditions which is passed to the machine learning model
 conditionals = np.array([[m1,m2,l1,l2]])
 
 cond = np.array([[m1,m2,l1,l2]])
-#print(cond)
 conditionals = np.vstack([m1_array,m2_array,l1_array,l2_array]).T
-
 conditionals = np.reshape(conditionals,
                           (conditionals.shape[0],conditionals.shape[1],1))
-#conditionals = np.array(conditionals)
-#print(conditionals.shape)
-#print(conditionals[0])
-#input()
+
+#scaling constants of the data. These were originally for the original data so it'd be better to programme in rather
+#than the given values here.
 g_scaling = -14.019296288484181
 r_scaling = -16.169625368881167
 i_scaling = -17.85982432553296
@@ -99,7 +73,7 @@ r_fname = "Models/Model_G4/model_r.pth"
 i_fname = "Models/Model_G4/model_i.pth"
 z_fname = "Models/Model_G4/model_z.pth"
 
-#get time arrays
+#get time arrays from dataframe created by the model programme
 g_f = "Data_Cache/New/Comp_120_Original_nannum.pkl"
 r_f = "Data_Cache/New/Comp_120_Original_nannum.pkl"
 i_f = "Data_Cache/New/Comp_120_Original_nannum.pkl"
@@ -115,6 +89,13 @@ t_r = np.vstack(rdata['time'])[0]
 t_i = np.vstack(idata['time'])[0]
 t_z = np.vstack(zdata['time'])[0]
 t_d = [t_g,t_r,t_i,t_z]
+
+#free up meamory
+gdata = 0
+rdata = 0
+idata = 0
+zdata = 0
+
 #Create flows
 g_flow = torch.load(g_fname)
 r_flow = torch.load(r_fname)
@@ -129,6 +110,7 @@ r_flow.to(device)
 i_flow.to(device)
 z_flow.to(device)
 
+#put flows in evaluation mode
 g_flow.eval()
 r_flow.eval()
 i_flow.eval()
@@ -143,19 +125,15 @@ I_Samples = []
 Z_Samples = []
 cond = torch.from_numpy(cond.astype(np.float32)).to(device)
 conditionals = torch.from_numpy(conditionals.astype(np.float32)).to(device)
-#print(conditionals)
+
 place = 0
 with torch.no_grad():
-        #for i in np.arange(N_samples):
-        
         for c in conditionals:
-            #print(c)
             if c[1] >= 1:
                 print("place:",place,"/",len(conditionals))
                 place += 1
                 c = c.T
-                #print(cond)
-                #print(conditionals)
+
                 g  = g_flow.sample(1,conditional = c)
                 r = r_flow.sample(1,conditional = c)
                 i = i_flow.sample(1,conditional = c)
@@ -178,8 +156,6 @@ Samples = [G_Samples,
 Ranges = []
 Maxes = []
 Mins = []
-
-
 Samples2 = [[],
             [],
             [],
@@ -191,7 +167,7 @@ for s in Samples:
         M = np.max(l)
         m = np.min(l)
         ran = M-m
-        if ran < 2e1:
+        if ran < 2e10:#the threshold for cropping. A useful tool when troubleshooting.
             Ranges.append(ran)
             Maxes.append(M)
             Mins.append(m)
@@ -199,8 +175,7 @@ for s in Samples:
     Samples2[i] = np.array(Samples2[i])
     i += 1
 
-#Samples2 = np.array(Samples2)
-#print(Samples2)      
+#plot histograms   
 plt.hist(Ranges,bins = 100)
 plt.title("Ranges")
 plt.yscale('log')
@@ -244,13 +219,13 @@ max_lines = [max_g,max_r,max_i,max_z]
 min_lines = [min_g,min_r,min_i,min_z]
 
 for i in np.arange(len(max_lines)):
-    #print(labels[i])
     col = cmap(i/len(max_lines))
     plt.plot(t_d[i],scales[i]*lines[i],"-",ms = 4, label = labels[i],c = col)
     plt.fill_between(t_d[i],min_lines[i]*scales[i],
                      max_lines[i]*scales[i],alpha = 0.2,
                      color = col)
 
+#plot the predictions with the cropped data
 plt.title(f'm1: {m1:.3g}, m2: {m2:.3g}, l1: {l1:.3g}, l2: {l1:.3g}')
 plt.gca().invert_yaxis()
 plt.legend()
